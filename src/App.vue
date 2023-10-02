814f850fa90b2be11c81d4d83a2f7ff8733fc15a
cedricfortuin <cedric.laurent.fortuin@gmail.com>
2023-10-02 14:08:56 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <div class="grid grid-cols-1 text-center mt-24 gap-y-4">
+    <h2 class="text-5xl">Welkom bij StatiegeldWijzer!</h2>
+    <p class="text-2xl max-w-4xl mx-auto">
+      <span class="text-emerald-500">StatiegeldWijzer</span> is er om informatie over statiegeld, afval en recycling
+      toegankelijk te maken voor iedereen.
+      Ontdek hoe statiegeld helpt onze planeet schoner te maken en hoe jij kunt bijdragen aan een duurzamere toekomst.
+    </p>
+
+    <p class="max-w-4xl mx-auto">
+      Onze website is momenteel in volle aanbouw. Kom later terug voor waardevolle informatie over statiegeld, afval en
+      recycling. Samen werken we aan een groenere toekomst!
+    </p>
+
+    <p class="mt-24">
+      Groetjes, <br/>Team <span class="text-emerald-500">StatiegeldWijzer</span>
+    </p>
+  </div>
+</template>
+
